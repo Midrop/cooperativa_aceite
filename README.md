ba9309b05232d45bd50475a11d2f1aa10dfc5dc1
jm-willy <jaimewilly@hotmail.es>
2024-10-07 16:28:40 +0200

    Primer

---
+++ README.md
@@ -0,0 +1,3 @@
+# App para cooperative de aceite de oliva
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/cooperativa_aceite/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cooperativa_aceite
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
