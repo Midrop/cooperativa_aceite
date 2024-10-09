1cae38ae75e32f4623fc49bba58c349b77552f91
jm-willy <jaimewilly@hotmail.es>
2024-10-09 13:22:29 +0200

    up

--- lib/login.dart
+++ lib/login.dart
@@ -6,6 +6,6 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
-  await FirebaseAuth.instance.customAuthDomain('url.firebaseapp.com', 9099);
+  await FirebaseAuth.instance.customAuthDomain('my-url.firebaseapp.com', 9099);
 //runApp(MyApp());
 }
