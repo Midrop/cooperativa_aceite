18dfe676e3bbb0acf15087b5517c400894dbde70
jm-willy <jaimewilly@hotmail.es>
2024-10-09 16:03:43 +0200

    up

--- lib/login.dart
+++ lib/login.dart
@@ -6,6 +6,11 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
-  await FirebaseAuth.instance.customAuthDomain('my-url.firebaseapp.com', 9099);
+  await FirebaseAuth.instance.useAuthEmulator('my-url.firebaseapp.com', 9099);
+//   await FirebaseAuth.instance.customAuthDomain!('my-url.firebaseapp.com');
 //runApp(MyApp());
 }
+
+
+// The function can't be unconditionally invoked because it can be 'null'.
+// Try adding a null check ('!')
