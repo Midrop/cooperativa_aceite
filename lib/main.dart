ed6b3c6332827ebfd75f361cf2031b06076ba2db
jm-willy <jaimewilly@hotmail.es>
2024-10-21 16:25:30 +0200

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'home.dart';
 import 'ofertas.dart';
 
 void main() {
@@ -30,142 +31,54 @@ class LandingPage extends StatefulWidget {
 }
 
 class LandingPageState extends State<LandingPage> {
-  int _selectedIndex = 1;
+  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    // var navBarIndex;
-
-    // Widget buildBody() {
-    //   switch (_selectedIndex) {
-    //     case 0:
-    //       return const Center(child: Text('Home Screen'));
-    //     case 1:
-    //       return const Center(child: Text('Search Screen'));
-    //     case 2:
-    //       return const MyHomePage(title: 'QWERTY');
-    //     default:
-    //       return const Center(child: Text('Home Screen'));
-    //   }
-    // }
-
     Widget page;
     switch (_selectedIndex) {
       case 0:
-        page = const MyHomePage(title: 'QWERTY');
-        return page;
-      // break;
+        page = const MyHomePage(title: 'PAGINA 0');
+        break;
       case 1:
-        page = const MyHomePage(title: 'QWERTY');
-        return page;
-      // break;
+        page = const HomePage(title: 'PAGINA 1');
+        break;
+      case 2:
+        page = const MyHomePage(title: 'PAGINA 2');
+        break;
       default:
         throw UnimplementedError('no widget for $_selectedIndex');
     }
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('INICIO'),
+        title: const Text('BARRA DE APLICACION, ICONO IZQUIRDA LOGIN/LOGOUT'),
         backgroundColor: Colors.teal,
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            // Imagen o logo
-            Container(
-              height: 300,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(
-                      'assets/landing_image.jpg'), // Añade tu imagen en assets
-                  fit: BoxFit.cover,
-                ),
-              ),
+      body: page,
+      bottomNavigationBar: NavigationBar(
+          onDestinationSelected: (int index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+          selectedIndex: _selectedIndex,
+          indicatorColor: const Color.fromARGB(116, 91, 120, 83),
+          backgroundColor: const Color.fromARGB(77, 116, 149, 102),
+          destinations: const <Widget>[
+            NavigationDestination(
+              icon: Icon(Icons.campaign),
+              label: 'Anuncios',
             ),
-
-            const SizedBox(height: 20),
-
-            // Título principal
-            const Padding(
-              padding: EdgeInsets.all(20.0),
-              child: Text(
-                'Bienvenidos',
-                style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                ),
-                textAlign: TextAlign.center,
-              ),
+            NavigationDestination(
+              icon: Icon(Icons.home),
+              label: 'Principal',
             ),
-
-            const SizedBox(height: 20),
-
-            // Texto descriptivo
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 30.0),
-              child: Text(
-                'Aquí puedes encontrar información relevante sobre nuestro producto.',
-                style: TextStyle(fontSize: 16),
-                textAlign: TextAlign.center,
-              ),
+            NavigationDestination(
+              icon: Icon(Icons.receipt),
+              label: 'Recibos',
             ),
-
-            const SizedBox(height: 20),
-
-            // Botón de llamada a la acción (CTA)
-            ElevatedButton(
-              onPressed: () {
-                // Acción del botón
-              },
-              style: ElevatedButton.styleFrom(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                backgroundColor: Colors.teal,
-              ),
-              child: const Text(
-                'LOGIN',
-                style: TextStyle(fontSize: 18),
-              ),
-            ),
-
-            const SizedBox(height: 20),
-
-            // Pie de página con enlaces
-            const Padding(
-              padding: EdgeInsets.all(20.0),
-              child: Text(
-                '© 2024 Cooperativa de aceite.',
-                style: TextStyle(fontSize: 14, color: Colors.grey),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            NavigationBar(
-                onDestinationSelected: (int index) {
-                  setState(() {
-                    _selectedIndex = index;
-                  });
-                },
-                selectedIndex: _selectedIndex,
-                indicatorColor: const Color.fromARGB(116, 91, 120, 83),
-                backgroundColor: const Color.fromARGB(77, 116, 149, 102),
-                // selectedIndex: 1,
-                destinations: const <Widget>[
-                  NavigationDestination(
-                    icon: Icon(Icons.campaign),
-                    label: 'Anuncios',
-                  ),
-                  NavigationDestination(
-                    icon: Icon(Icons.home),
-                    label: 'Principal',
-                  ),
-                  NavigationDestination(
-                    icon: Icon(Icons.receipt),
-                    label: 'Recibos',
-                  ),
-                ])
-          ],
-        ),
-      ),
+          ]),
     );
   }
 }
